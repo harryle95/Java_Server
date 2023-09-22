efd1f308ea06c29039e60862fab7128d04d8fc33
hson <lehoangsonsg@gmail.com>
2023-09-22 10:30:41 +0930

    feat: add regex for matching key: value remove enclosing " from value

--- src/src/utility/json/Parser.java
+++ src/src/utility/json/Parser.java
@@ -1,20 +1,26 @@
 package utility.json;
 
-public class Parser {
-    public WeatherData parseFile(String fileName) {
-        return null;
-    }
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    public WeatherData parseString(String message) {
-        String[] splitMsg = message.split("\n");
+public class Parser {
+    public static final Pattern pattern = Pattern.compile(
+            "\"(\\w+)\": ?(\"[^\"]*\"|[^,\\n}]+)(,|\\n|})"
+    );
+
+    public static WeatherData parseString(String message) {
         WeatherData data = new WeatherData();
-        for (String msg : splitMsg) {
-            if (msg.equals("{") | msg.equals("}")) {
-                continue;
-            }
-            String[] line = msg.split(": ");
-            data.put(line[0], line[1]);
+        Matcher matcher = pattern.matcher(message);
+        while (matcher.find()) {
+            String value = matcher.group(2);
+            if (value.charAt(0) == '"' && value.charAt(value.length() - 1) == '"')
+                value = value.substring(1, value.length() - 1);
+            data.put(matcher.group(1), value);
         }
         return data;
+    }
+
+    public WeatherData parseFile(String fileName) {
+        return null;
     }
 }
