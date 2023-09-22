f95d27981d217aa0041da7e3d347360b21f2562e
hoangson <lehoangsonsg@gmail.com>
2023-09-23 00:43:17 +0930

    test: add content server format message test

--- src/test/utility/domain/ContentServerParserTest.java
+++ src/test/utility/domain/ContentServerParserTest.java
@@ -1,6 +1,7 @@
 package utility.domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -46,4 +47,6 @@ class ContentServerParserTest {
         String[] argv = args.split(" ");
         assertThrows(RuntimeException.class, () -> parser.parse(argv));
     }
+
+
 }
