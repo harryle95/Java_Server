09a28452786232cb4dca8549b2d2369da4cd25c2
hson <lehoangsonsg@gmail.com>
2023-09-22 13:55:47 +0930

    chore: remove unused imports

--- src/test/utility/json/WeatherDataTest.java
+++ src/test/utility/json/WeatherDataTest.java
@@ -1,11 +1,9 @@
 package utility.json;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
