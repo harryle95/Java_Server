1942730c0254ef2428e80ab2c3da51f69252fd26
hson <lehoangsonsg@gmail.com>
2023-09-22 21:24:34 +0930

    feat: make SocketClient abstract, simplify constructor

--- src/src/SocketClient.java
+++ src/src/SocketClient.java
@@ -5,11 +5,17 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class SocketClient {
+public abstract class SocketClient {
     Socket clientSocket;
     PrintWriter out;
     BufferedReader in;
-    private String URL;
+
+    private String serverName;
+
+    private int port;
+
+    public SocketClient(String[] argv) {
+    }
 
     public String getServerName() {
         return serverName;
@@ -17,32 +23,6 @@ public class SocketClient {
 
     public int getPort() {
         return port;
-    }
-
-    private String serverName;
-
-    private int port;
-
-    public void parse(String URL) {
-        String[] split_string = URL.split(":");
-        if (split_string.length == 2) {
-            this.serverName = split_string[0];
-            this.port = Integer.parseInt(split_string[1]);
-        } else if (split_string.length != 3) {
-            throw new RuntimeException("URL must be server:port or http://server:port or http://server.domain:port");
-        } else {
-            this.port = Integer.parseInt(split_string[2]);
-            String _serverName = split_string[1].split("\\.")[0];
-            if (!_serverName.startsWith("//")) {
-                throw new RuntimeException("URL must be server:port or http://server:port or http://server.domain:port");
-            }
-            this.serverName = _serverName.substring(2);
-        }
-        System.out.println(serverName + ":" + port);
-    }
-
-    public SocketClient(String URL) {
-        parse(URL);
     }
 
     public void connect() {
