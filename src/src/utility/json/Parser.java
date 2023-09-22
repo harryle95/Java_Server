47e3a28770ee49b228036050f8a1ae3e95ece239
hson <lehoangsonsg@gmail.com>
2023-09-22 11:49:36 +0930

    feat: allow parsing multiple IDs in message body

--- src/src/utility/json/Parser.java
+++ src/src/utility/json/Parser.java
@@ -1,5 +1,7 @@
 package utility.json;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,16 +10,31 @@ public class Parser {
             "\"(\\w+)\": ?(\"[^\"]*\"|[^,\\n}]+)(,|\\n|})"
     );
 
-    public static WeatherData parseString(String message) {
+    public static Map<String, WeatherData> parseString(String message) {
+        Map<String, WeatherData> container = new LinkedHashMap<>();
         WeatherData data = new WeatherData();
         Matcher matcher = pattern.matcher(message);
         while (matcher.find()) {
+            String key = matcher.group(1);
             String value = matcher.group(2);
+
+            // Process Value
             if (value.charAt(0) == '"' && value.charAt(value.length() - 1) == '"')
                 value = value.substring(1, value.length() - 1);
-            data.put(matcher.group(1), value);
+
+            // Check if continue old entry or is new entry
+            if (data.containsKey(key)) {
+                if (data.containsKey("id"))
+                    container.put(data.getID(), data); // Items only valid if has ID
+                data = new WeatherData();
+            }
+            // Put Value
+            data.put(key, value);
         }
-        return data;
+        // Put the last item if valid
+        if (data.containsKey("id"))
+            container.put(data.getID(), data);
+        return container;
     }
 
     public WeatherData parseFile(String fileName) {
