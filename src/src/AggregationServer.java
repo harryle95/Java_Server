484976e8c8a73e87d0f5d5a55647faded16fbf8d
hoangson <lehoangsonsg@gmail.com>
2023-09-24 22:40:08 +0930

    feat: add methods to handle requests. Incomplete

--- src/src/AggregationServer.java
+++ src/src/AggregationServer.java
@@ -1,3 +1,8 @@
+import utility.http.HTTPRequest;
+import utility.http.HTTPResponse;
+import utility.http.MessageExchanger;
+
+import java.beans.ExceptionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,9 +11,17 @@ import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.Base64;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class AggregationServer {
     private ServerSocket serverSocket;
+    private final ExecutorService connRecvPool; // Threadpool to accept incoming requests
+
+    public AggregationServer() {
+        connRecvPool = Executors.newFixedThreadPool(10);
+    }
 
     public void run(int port) throws IOException {
         serverSocket = new ServerSocket(port);
@@ -18,11 +31,10 @@ public class AggregationServer {
 
     public void start() {
         while (true) {
-            try (
-                    Socket clientSocket = serverSocket.accept();
-            ) {
-                ClientHandler handler = new ClientHandler(clientSocket);
-                new Thread(handler).start();
+            try {
+                Socket clientSocket = serverSocket.accept();
+                RequestHandler handler = new RequestHandler(clientSocket);
+                connRecvPool.execute(handler);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -56,39 +68,69 @@ public class AggregationServer {
     }
 }
 
-class ClientHandler implements Runnable {
-    private final Socket clientSocket;
 
-    public ClientHandler(Socket socket) {
+class RequestHandler implements Runnable {
+    private final Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    // TODO: shared Lamport-Clock object to update timing
+    public RequestHandler(Socket socket) throws IOException {
         this.clientSocket = socket;
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        System.out.println("Connecting to: " + clientSocket.getRemoteSocketAddress());
+    }
+
+    private String handleGET(HTTPRequest request) {
+        String body;
+        if (request.uri.equals("/")) {
+            body = "";
+        } else {
+            // TODO: check if StationID exists in Map
+            body = request.uri.substring(1);
+        }
+        HTTPResponse response = new HTTPResponse("1.1")
+                .setStatusCode("200")
+                .setReasonPhrase("OK")
+                .setHeader("Content-Type", "application/json")
+                .setBody(body);
+        return response.build();
+    }
+
+    private String handlePUT(HTTPRequest request) {
+        String body = request.body;
+        // TODO: perform correct PUT
+        HTTPResponse response = new HTTPResponse("1.1")
+                .setStatusCode("200")
+                .setReasonPhrase("OK")
+                .setHeader("Content-Type", "application/json")
+                .setBody(body);
+        return response.build();
+    }
+
+
+    private String getResponse(HTTPRequest request) {
+        if (request.method.equals("GET"))
+            return handleGET(request);
+        if (request.method.equals("PUT"))
+            return handlePUT(request);
+        return new HTTPResponse("1.1").setStatusCode("400").setReasonPhrase("Bad Request").build();
     }
 
     @Override
     public void run() {
-        try (
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        ) {
-            Thread currentThread = Thread.currentThread();
-            String name = currentThread.getName();
-            long id = currentThread.getId();
-            String threadInfo = name + "." + String.valueOf(id);
-            System.out.println("Spawning a new socket: " + clientSocket.getLocalPort());
-            System.out.println("Accepting Connection from: " + clientSocket.getRemoteSocketAddress());
-            System.out.println("ThreadID: " + threadInfo);
-            String clientInput, serverInput;
-            while (true) {
-                clientInput = in.readLine();
-                System.out.println(clientInput);
-                out.println("Server Response");
-//                if ((clientInput == null) | (clientInput != null && clientInput.equals("exit"))) {
-//                    break;
-//                }
-//                if (!clientInput.isEmpty()) {
-//                    System.out.println(threadInfo+ ">>" + clientInput);
-//                    out.println(clientInput);
-//                }
+        try {
+            String message;
+            while ((message = in.readLine()) != null) {
+                HTTPRequest request = HTTPRequest.fromMessage(MessageExchanger.decode(message));
+                // TODO: submit request to a task queue and get the Future as a CompletionService
+                System.out.println(request.build());
+                String response = getResponse(request);
+                out.println(MessageExchanger.encode(response));
             }
+            System.out.println("Socket is closed");
+            clientSocket.close();
         } catch (SocketException e) {
             System.out.println("Connection aborted. Client is disconnected");
         } catch (IOException e) {
