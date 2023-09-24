bc29a18ca6beb083ab29cea5e697f81ac159a78b
hoangson <lehoangsonsg@gmail.com>
2023-09-24 22:39:40 +0930

    feat: message exchange works. Will need refactoring

--- src/src/GETClient.java
+++ src/src/GETClient.java
@@ -1,6 +1,11 @@
 import utility.domain.GETClientParser;
 import utility.domain.GETServerInformation;
 import utility.http.HTTPRequest;
+import utility.http.MessageExchanger;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class GETClient extends SocketClient {
     private String stationID;
@@ -12,7 +17,6 @@ public class GETClient extends SocketClient {
         setPort(info.port);
         setStationID(info.stationID);
     }
-
 
     public String getStationID() {
         return stationID;
@@ -31,10 +35,40 @@ public class GETClient extends SocketClient {
             request.setURI("/" + stationID);
 
         //Add Hostname
-        request.setHeader("Host", getHostname()+":"+getPort());
+        request.setHeader("Host", getHostname() + ":" + getPort());
 
         //Accept Json
         request.setHeader("Accept", "application/json");
         return request;
+    }
+
+    public void run() {
+        try {
+            connect();
+            HTTPRequest request = formatMessage();
+            System.out.println(request.build());
+            out.println(request.build());
+            String response;
+            while ((response = in.readLine()) != null) {
+                System.out.println(response);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            close();
+        }
+    }
+
+    public static void main(String[] argv) throws IOException {
+        GETClient client = new GETClient(argv);
+        client.connect();
+        HTTPRequest request = client.formatMessage();
+        System.out.println(request.build());
+        client.out.println(MessageExchanger.encode(request.build()));
+        while (true) {
+            String output = client.in.readLine();
+            System.out.println(MessageExchanger.decode(output));
+            break;
+        }
     }
 }
