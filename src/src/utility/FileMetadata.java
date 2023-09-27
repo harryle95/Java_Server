cf388e65248ba3ee6eb7dfff07b95ee08032d672
hoangson <lehoangsonsg@gmail.com>
2023-09-28 09:10:41 +0930

    feat: add timestamp field in metadata to distinguish fresh vs stale file

--- src/src/utility/FileMetadata.java
+++ src/src/utility/FileMetadata.java
@@ -1,10 +1,15 @@
+package utility;
+
 public class FileMetadata {
     private final String remoteIP;
     private final String fileName;
 
-    public FileMetadata(String remoteIP, String fileName) {
+    private final String timestamp;
+
+    public FileMetadata(String remoteIP, String fileName, String timestamp) {
         this.remoteIP = remoteIP;
         this.fileName = fileName;
+        this.timestamp = timestamp;
     }
 
     public String getFileName() {
@@ -13,5 +18,9 @@ public class FileMetadata {
 
     public String getRemoteIP() {
         return remoteIP;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
     }
 }
