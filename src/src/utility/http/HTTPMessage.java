2e3a2e1de39c4c071fb4af9c944070e025821e46
hoangson <lehoangsonsg@gmail.com>
2023-09-28 10:07:56 +0930

    doc: add docstring for HTTPMessage

--- src/src/utility/http/HTTPMessage.java
+++ src/src/utility/http/HTTPMessage.java
@@ -2,7 +2,20 @@ package utility.http;
 
 public interface HTTPMessage {
 
+    /**
+     * Set message header
+     *
+     * @param key   header key
+     * @param value header value
+     * @return HTTPMessage message
+     */
     public HTTPMessage setHeader(String key, String value);
 
+
+    /**
+     * Generate HTTPMessage string based on builder
+     *
+     * @return HTTPMessage as string
+     */
     public String toString();
 }
