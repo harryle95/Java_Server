48d70283e1c16ddb99994c9e72dabc6cb8df8044
hoangson <lehoangsonsg@gmail.com>
2023-09-28 21:30:58 +0930

    feat: add TODO Reminder

--- src/src/AggregationServer.java
+++ src/src/AggregationServer.java
@@ -13,6 +13,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.*;
 
+//TODO - Add BackUpRunnable
+//TODO - Add HeartBeat/HealthCheck Runnable
 public class AggregationServer {
     private final ConcurrentMap<String, String> database;
     private final ConcurrentMap<String, ConcurrentMap<String, ConcurrentMap<String, String>>> archive;
