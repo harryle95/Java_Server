bf250504d1a6562eef6e179330f26086d4703592
hoangson <lehoangsonsg@gmail.com>
2023-09-29 19:15:41 +0930

    feat: add composite ID test cases

--- src/test/IntegrationTest.java
+++ src/test/IntegrationTest.java
@@ -68,7 +68,6 @@ public abstract class IntegrationTest {
     }
 }
 
-
 class OneGetOneContentTest extends IntegrationTest {
 
     @Test
@@ -310,7 +309,7 @@ class OneGetOneContentTest extends IntegrationTest {
     }
 
     @Test
-    void testClientSendingPOSTRequestReceiveBadRequest() throws IOException{
+    void testClientSendingPOSTRequestReceiveBadRequest() throws IOException {
         GETClient client = GETClient.from_args("127.0.0.1:4567".split(" "));
         HTTPRequest request = new HTTPRequest("1.1").setMethod("POST").setURI("/Adelaide");
         client.send(request);
@@ -401,7 +400,7 @@ class MultipleSerialPUTTest extends IntegrationTest {
 
     @Test
     void testOldestEventsEjectedFromArchive() throws IOException {
-        for (String file: fileNames){
+        for (String file : fileNames) {
             ContentServer.main(("127.0.0.1:4567 " + file).split(" "));
         }
         assertFalse(server.getArchive().get("/127.0.0.1").containsKey(fileNames.get(0)));
@@ -418,5 +417,143 @@ class MultipleSerialPUTTest extends IntegrationTest {
         assertFalse(server.getArchive().get("/127.0.0.1").containsKey(fileNames.get(0)));
     }
 
+    @Test
+    void testWhenUpdateQueueContainsTheSameEntryArchiveDoNotRemove() throws IOException {
+        server.setFRESHCOUNT(1);
+        ContentServer.main(("127.0.0.1:4567 " + fileNames.get(0)).split(" "));
+        ContentServer.main(("127.0.0.1:4567 " + fileNames.get(0)).split(" "));
+        assertTrue(server.getArchive().get("/127.0.0.1").containsKey(fileNames.get(0)));
+    }
+
+    @Test
+    void testWhenUpdateQueueContainsUpdatedVersionOfTheSameEntryArchiveDoNotRemove() throws IOException {
+        server.setFRESHCOUNT(3);
+        ContentServer.main(("127.0.0.1:4567 " + fileNames.get(0)).split(" "));
+        ContentServer.main(("127.0.0.1:4567 " + fileNames.get(2)).split(" "));
+        ContentServer.main(("127.0.0.1:4567 " + fileNames.get(1)).split(" "));
+        ContentServer.main(("127.0.0.1:4567 " + fileNames.get(0)).split(" "));
+        assertTrue(server.getArchive().get("/127.0.0.1").containsKey(fileNames.get(0)));
+    }
+
+    @Test
+    void testAllFilesRemovedSteadyState() throws InterruptedException {
+        server.setWAITTIME(1);
+        Runnable task1 = () -> {
+            try {
+                ContentServer.main(("127.0.0.1:4567 " + fileNames.get(0)).split(" "));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+
+        Runnable task2 = () -> {
+            try {
+                ContentServer.main(("127.0.0.1:4567 " + fileNames.get(1)).split(" "));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+
+        Runnable task3 = () -> {
+            try {
+                ContentServer.main(("127.0.0.1:4567 " + fileNames.get(3)).split(" "));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+
+        Runnable task4 = () -> {
+            try {
+                ContentServer.main(("127.0.0.1:4567 " + fileNames.get(4)).split(" "));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+
+        new Thread(task1).start();
+        new Thread(task2).start();
+        new Thread(task3).start();
+        new Thread(task4).start();
+
+        Thread.sleep(2000);
+        assertTrue(server.getArchive().get("/127.0.0.1").isEmpty());
+    }
+
+    @Test
+    void testPUTRequestAreSerialisedCorrectlyFourTasks() throws InterruptedException, IOException {
+        ContentServer.main(("127.0.0.1:4567 " + fileNames.get(0)).split(" "));
+        ContentServer.main(("127.0.0.1:4567 " + fileNames.get(1)).split(" "));
+        ContentServer.main(("127.0.0.1:4567 " + fileNames.get(2)).split(" "));
+        ContentServer.main(("127.0.0.1:4567 " + fileNames.get(3)).split(" "));
+
+        GETClient firstClient = GETClient.from_args("127.0.0.1:4567 5000".split(" "));
+        firstClient.run();
+        assertEquals(HTTPResponse.fromMessage(firstClient.receivedMessages.get(0)).body, fixtureMap.get(fileNames.get(1)));
+
+        GETClient secondClient = GETClient.from_args("127.0.0.1:4567 5045".split(" "));
+        secondClient.run();
+        assertEquals(HTTPResponse.fromMessage(secondClient.receivedMessages.get(0)).body, fixtureMap.get(fileNames.get(3)));
+
+
+    }
+
+}
+
+class MultiplePUTWithCompositeDataTest extends IntegrationTest{
+    Map<String, String> fixtureMap;
+
+    List<String> fileNames;
+    List<String> fileNamesComposite;
+    @BeforeEach
+    void createFixture() throws IOException {
+        fixtureMap = new HashMap<>();
+        fileNames = new ArrayList<>();
+        fileNamesComposite = new ArrayList<>();
+
+        Parser parser = new Parser();
+        fileNames.add("src/resources/SingleEntry/Adelaide_2023-07-15_16-30-00.txt");
+        fileNames.add("src/resources/SingleEntry/Glenelg_2023-07-15_16-30-00.txt");
+        fileNames.add("src/resources/SingleEntry/HenleyBeach_2023-07-15_16-30-00.txt");
+        fileNames.add("src/resources/SingleEntry/Glenelg_2023-07-15_16-00-00.txt");
+
+        parser.parseFile(Path.of(fileNames.get(0)));
+        fixtureMap.put("5000", parser.toString());
+
+        parser.parseFile(Path.of(fileNames.get(1)));
+        fixtureMap.put("5045_New", parser.toString());
+
+        parser.parseFile(Path.of(fileNames.get(2)));
+        fixtureMap.put("5022", parser.toString());
+
+        parser.parseFile(Path.of(fileNames.get(3)));
+        fixtureMap.put("5045_Old", parser.toString());
+
+        fileNamesComposite.add("src/resources/Composite/Adelaide_2023-07-15_16-30-00.txt");
+        fileNamesComposite.add("src/resources/Composite/Glenelg_2023-07-15_16-30-00.txt");
+    }
+
+    @Test
+    void testCompositePUTUseLatestTimer() throws IOException {
+        ContentServer.main(("127.0.0.1:4567 " + fileNamesComposite.get(0)).split(" "));
+        GETClient firstClient = GETClient.from_args("127.0.0.1:4567 5000".split(" "));
+        firstClient.run();
+        assertEquals(HTTPResponse.fromMessage(firstClient.receivedMessages.get(0)).body, fixtureMap.get("5000"));
+        ContentServer.main(("127.0.0.1:4567 " + fileNamesComposite.get(1)).split(" "));
+        GETClient secondClient = GETClient.from_args("127.0.0.1:4567 5022".split(" "));
+        secondClient.run();
+        assertEquals(HTTPResponse.fromMessage(secondClient.receivedMessages.get(0)).body, fixtureMap.get("5022"));
+    }
+
+    @Test
+    void testConsecutivePUTOnSameIDUseLatestUpdate() throws IOException {
+        ContentServer.main(("127.0.0.1:4567 " + fileNamesComposite.get(0)).split(" "));
+        GETClient firstClient = GETClient.from_args("127.0.0.1:4567 5045".split(" "));
+        firstClient.run();
+        assertEquals(HTTPResponse.fromMessage(firstClient.receivedMessages.get(0)).body, fixtureMap.get("5045_Old"));
+        ContentServer.main(("127.0.0.1:4567 " + fileNamesComposite.get(1)).split(" "));
+        GETClient secondClient = GETClient.from_args("127.0.0.1:4567 5045".split(" "));
+        secondClient.run();
+        assertEquals(HTTPResponse.fromMessage(secondClient.receivedMessages.get(0)).body, fixtureMap.get("5045_New"));
+    }
 
 }
