0f667f991a43830a158058d2f176dffee6d21f2c
hoangson <lehoangsonsg@gmail.com>
2023-09-30 00:42:14 +0930

    feat: add connection retry with timeout

--- src/src/utility/SocketClient.java
+++ src/src/utility/SocketClient.java
@@ -1,15 +1,49 @@
 package utility;
 
+import utility.http.HTTPRequest;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
 
 public class SocketClient extends SocketCommunicator {
+    private int retry = 0;
+
+    public void setMAX_RETRY(int MAX_RETRY) {
+        this.MAX_RETRY = MAX_RETRY;
+    }
+
+    private int SO_TIMEOUT = 5000;
+
+    private int MAX_RETRY = 5;
+
     public SocketClient(
             Socket clientSocket,
             PrintWriter out,
-            BufferedReader in) {
+            BufferedReader in) throws SocketException {
         super(clientSocket, new LamportClock(), out, in, "client");
+        clientSocket.setSoTimeout(SO_TIMEOUT);
+    }
+
+    public String receive() throws IOException {
+        // GETClient and ContentServer can resend messages up to 5 times
+        try {
+            String message = super.receive();
+            retry = 0;
+            return message;
+        } catch (SocketTimeoutException e) {
+            retry += 1;
+            if (retry < MAX_RETRY) {
+                send(HTTPRequest.fromMessage(sentMessages.get(sentMessages.size() - 1)));
+                return receive();
+            } else {
+                retry = 0;
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     protected String hostname;
@@ -22,5 +56,10 @@ public class SocketClient extends SocketCommunicator {
 
     public int getPort() {
         return port;
+    }
+
+    public void setSO_TIMEOUT(int SO_TIMEOUT) throws SocketException {
+        this.SO_TIMEOUT = SO_TIMEOUT;
+        clientSocket.setSoTimeout(SO_TIMEOUT);
     }
 }
