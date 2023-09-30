8adafd6c729e788c739492703237a54107f29543
hson <lehoangsonsg@gmail.com>
2023-09-30 14:56:51 +0930

    feat: add xml report

--- src/report/ReportGenerator.java
+++ src/report/ReportGenerator.java
@@ -8,7 +8,8 @@ import java.util.List;
 
 public class ReportGenerator {
     public static void main(String[] argv) throws IOException {
-        File htmlPath = new File("report"); // path to the output HTML directory
+        File htmlPath = new File("report/html"); // path to the output HTML directory
+        File xmlPath = new File("report/coverage.xml"); // path to the output HTML directory
         String title = "Code Coverage Report"; // title of the HTML document
         File icPath = new File("src/report/report.ic"); // path to the report.ic
         List<File> reports = Collections.singletonList(icPath);
@@ -18,8 +19,10 @@ public class ReportGenerator {
                 "/Java_Server"));
         Filters filters = Filters.EMPTY;
 
+        // Generate XML Report
+        ReportApi.xmlReport(xmlPath, reports, outputRoots, sourceRoots, filters);
+        // Generate HTML Report
         ReportApi.htmlReport(htmlPath, title, null, reports, outputRoots,
                 sourceRoots, filters);
-
     }
 }
