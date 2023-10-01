d5c20ea78331155f74a1e1afacdce6d17c738c87
hson <lehoangsonsg@gmail.com>
2023-10-01 15:50:23 +1030

    feat: update logging example

--- src/src/LoggingExample.java
+++ src/src/LoggingExample.java
@@ -1,14 +1,19 @@
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.logging.*;
 
 public class LoggingExample {
-    private static final Logger logger = Logger.getLogger("MainThreadLogger");
+    static final Logger logger = Logger.getLogger(LoggingExample.class.getName());
+
+    ExecutorService threadPool;
 
     public LoggingExample() throws IOException {
-        FileHandler handler = new FileHandler("src/log/server.log", 50000, 10, false);
+        FileHandler handler = new FileHandler("src/log/server.log", 50000, 10, true);
         handler.setFormatter(new XMLFormatter());
         handler.setLevel(Level.INFO);
         logger.addHandler(handler);
+        threadPool = Executors.newCachedThreadPool();
     }
 
     public void logOnMainThread(){
@@ -16,19 +21,30 @@ public class LoggingExample {
         System.out.println("Running method on main thread");
     }
 
+    public void submit(Runnable task){
+        threadPool.submit(task);
+    }
+
     public static void main(String[] argv) throws IOException {
         logger.info("Starting main program");
         LoggingExample program = new LoggingExample();
         program.logOnMainThread();
-        new Thread(new RunningThread()).start();
-    }
-
-    static class RunningThread implements Runnable {
-        @Override
-        public void run() {
-            logger.info("Logging Inside another thread");
-            System.out.println("Running method on thread");
+        for (int i = 0; i < 10; i++){
+            program.submit(new RunningThread());
         }
+
     }
 
+
+
+}
+
+class RunningThread implements Runnable {
+    private static final Logger logger = Logger.getLogger(RunningThread.class.getName());
+    @Override
+    public void run() {
+        long id = Thread.currentThread().threadId();
+        logger.info("Logging Inside thread:" + id);
+        System.out.println("Running method on thread: " + id);
+    }
 }
