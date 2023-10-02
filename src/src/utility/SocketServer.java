ed690480106f05169af0c6985fb3bdbccb11d208
hoangson <lehoangsonsg@gmail.com>
2023-10-02 11:03:57 +1030

    feat: add ABC socketserver

---
+++ src/src/utility/SocketServer.java
@@ -0,0 +1,54 @@
+package utility;
+
+import annotations.IgnoreCoverage;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.logging.Logger;
+
+public abstract class SocketServer {
+    protected final Logger logger;
+    private final int port;
+    protected ServerSocket serverSocket;
+
+
+    public boolean isUp() {
+        return isUp;
+    }
+
+    protected boolean isUp;
+
+    public SocketServer(int port) {
+        logger = Logger.getLogger(this.getClass().getName());
+        this.port = port;
+        this.isUp = true;
+    }
+
+    public static int getPort(String[] args) {
+        int port;
+        if (args.length == 1) {
+            port = Integer.parseInt(args[0]);
+        } else if (args.length == 0) {
+            port = 4567;
+        } else {
+            throw new RuntimeException("Usage: AggregationServer [port].");
+        }
+        return port;
+    }
+
+
+    public void run() throws IOException {
+        serverSocket = new ServerSocket(port);
+        serverSocket.setReuseAddress(true);
+        logger.info("Server listens to port " + port);
+    }
+
+    public abstract void start() throws IOException;
+
+    public void close() throws IOException {
+        logger.info("Closing Server");
+        serverSocket.close();
+        logger.info("Server is closed");
+        isUp = false;
+    }
+}
